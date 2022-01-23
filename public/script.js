5d390314c8cee520569c15a4dd256e938d2aaad1
MadsRF <madsrunefrederiksen@gmail.com>
2022-01-23 11:50:10 +0100

    change to deploy

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 // with strict mode, you can not, for example, use undeclared variables. ex: x = 3.14; 
 'use strict'
 
-//export const ROOT_URL = ""; // heroku
-export const ROOT_URL = "http://localhost/exam/"; // local
+export const ROOT_URL = ""; // heroku
+//export const ROOT_URL = "http://localhost/exam/"; // local
 
 
